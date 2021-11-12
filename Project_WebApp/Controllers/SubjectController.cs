6bd0d87d83c44d07e0bee825743b938787b811d9
Yari Mariën <r0864345@student.thomasmore.be>
2021-11-12 12:34:39 +0100

    bug fixes

--- Project_WebApp/Controllers/SubjectController.cs
+++ Project_WebApp/Controllers/SubjectController.cs
@@ -7,7 +7,6 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using static Project_WebApp.ViewModels.FilterViewModel;
 
 namespace Project_WebApp.Controllers
 {
