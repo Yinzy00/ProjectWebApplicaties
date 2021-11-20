16040ba75d92b004d4986191e8c550f84c60ffaf
Yari Mariën <yari.marien@telenet.be>
2021-11-20 03:12:54 +0100

    Fixed includes for viewmodel

--- Project_WebApp/Data/Repositories/PostRepository.cs
+++ Project_WebApp/Data/Repositories/PostRepository.cs
@@ -22,12 +22,21 @@ namespace Project_WebApp.Data.Repositories
             return list;
         }
         public Post GetPostByIdForPostViewMode(int id)
+        
         {
             var post = _dbContext.Set<Post>()
                 .Include(p => p.User)
-                .Include(p => p.Comments)//Layer 1
-                .ThenInclude(c=>c.Comments)//Layer 2
-                .ThenInclude(c=>c.Comments)//Layer 3 (Max layer)
+                .Include(p => p.Likes)
+                .Include(p => p.Comments)
+                .ThenInclude(c=>c.Likes)//Layer 1
+                .Include(p=>p.Comments)
+                .ThenInclude(c=>c.Comments)
+                .ThenInclude(c => c.Likes)
+                //.ThenInclude(c=>c.Likes)//Layer 2
+                .Include(p => p.Comments)
+                .ThenInclude(c => c.Comments)
+                .ThenInclude(c=>c.Comments)
+                .ThenInclude(c => c.Likes)//Layer 3 (Max layer)
                 .Include(p => p.MessageImages)
                 .Include(p => p.PostSubjects)
                 .ThenInclude(ps => ps.Subject)
